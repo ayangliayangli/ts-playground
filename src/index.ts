79c21eecca9fa2ef0444d8b073f6246e78dc8153
yangfee <a@yangfee.com>
2021-09-09 15:40:16 +0800

    小型ts项目初始化

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { add } from './demo';
+
+const data: string = 'selfstudyfe';
+console.log(`1 + 1 = ${add(1, 1)}`);
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { resolve } = require('path');
+
+module.exports = {
+  entry: {
+    main: resolve('src/index.ts'),
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  devtool: 'source-map',// 打包出的js文件是否生成map文件（方便浏览器调试）
+};
